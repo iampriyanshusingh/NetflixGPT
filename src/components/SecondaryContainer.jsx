68e334500184825ccda73a85ecbd66967591a89f
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-04-15 00:56:07 +0530

    `added diff slices of topRated && upcoming && popular`

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -8,10 +8,9 @@ const SecondaryContainer = () => {
     <div className=" bg-black">
       <div className="-mt-44 pl-12 relative z-20">
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
+        <MovieList title={"Upcoming"} movies={movies.upcomingMovies} />
         <MovieList title={"Popular"} movies={movies.popularMovies} />
-        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
       </div>
     </div>
   );
--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -10,6 +10,7 @@ const movieSlice = createSlice({
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
+
     addTrailerVideo: (state, action) => {
       state.trailerVideo = action.payload;
     },
@@ -17,9 +18,23 @@ const movieSlice = createSlice({
     addPopularMovies: (state, action) => {
       state.popularMovies = action.payload;
     },
+
+    addTopRatedMovies: (state, action) => {
+      state.topRatedMovies = action.payload;
+    },
+
+    addUpcomingMovies: (state, action) => {
+      state.upcomingMovies = action.payload;
+    },
   },
 });
 
-export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies } =
-  movieSlice.actions;
+export const {
+  addNowPlayingMovies,
+  addTrailerVideo,
+  addPopularMovies,
+  addTopRatedMovies,
+  addUpcomingMovies,
+} = movieSlice.actions;
+movieSlice.actions;
 export default movieSlice.reducer;
