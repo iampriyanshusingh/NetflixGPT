47e29833174afead15df4691b0d7902127f8c5eb
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-04-14 22:03:18 +0530

    `fetched movie API and done importing trailer video`

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import useTrailerVideo from "../hooks/useTrailerVideo";
 
-const VideoBackground = () => {
-  return <div></div>;
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+
+  useTrailerVideo(movieId);
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
---
+++ src/hooks/useTrailerVideo.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { API_NOW_PLAYING } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/movieSlice";
+
+const useTrailerVideo = (movieId) => {
+  const dispatch = useDispatch();
+
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_NOW_PLAYING
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+};
+
+export default useTrailerVideo;
